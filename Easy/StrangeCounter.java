7a1f40793436aa2d69d95e6731f0ae9279c93bea
Roman Iefimov <romaniefofficial@gmail.com>
2018-09-10 22:42:35 -0700

    Strange  Counter solved

---
+++ Easy/StrangeCounter.java
@@ -0,0 +1,43 @@
+// Strange Counter
+// https://www.hackerrank.com/challenges/strange-code/problem
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class StrangeCounter {
+
+    // Complete the strangeCounter function below.
+    static long strangeCounter(long t) {
+        long v = (long)Math.ceil(t/(float)3);
+        v |= v >> 1;
+        v |= v >> 2;
+        v |= v >> 4;
+        v |= v >> 8;
+        v |= v >> 16;
+        v++;
+        v /= 2;
+        return (3 * v) - (t - 3 * (v-1)) + 1;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        long t = scanner.nextLong();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long result = strangeCounter(t);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
